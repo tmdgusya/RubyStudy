0f30d869259ec34e072c4d2c356da51a1987145c
tmdgusya <tmdgusya@naver.com>
2021-05-04 11:59:15 +0900

    ruby val

---
+++ dt.rb
@@ -0,0 +1,23 @@
+# number
+num = 2
+
+# String
+string1 = "Hello Ruby"
+string2 = 'Hello Ruby'
+
+# null
+
+isnill = nil
+
+# True Or False
+
+a = true
+b = false
+
+
+puts num
+puts string1
+puts string2
+puts isnill
+puts a
+puts b
