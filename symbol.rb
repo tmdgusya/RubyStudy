3f2d450c0057983d98d72e74e0915d2dc299a1e2
tmdgusya <tmdgusya@naver.com>
2021-05-04 23:21:26 +0900

    symbol hash

--- symbol.rb
+++ symbol.rb
@@ -5,3 +5,27 @@ def walk(direction)
 end
 
 puts walk(:north)
+
+inst_section = {
+  cello:  'string',
+  clarinet:  'woodwind',
+  drum: 'percussion',
+  oboe: 'woodwind',
+  trumpet: 'brass',
+  violin: 'string'
+}
+puts inst_section[:oboe]
+
+puts :abcd.object_id
+
+puts "abcd" == "abcd"
+
+# 우리가 심볼을 써야하는 이유 Ruby 는 계속 Object 를 생성함.
+puts "abcd".object_id == "abcd".object_id
+
+# Symbol 을 사용하는 이유
+puts :abcd.object_id == :abcd.object_id
+
+zzz = "abcd"
+
+puts zzz.intern
